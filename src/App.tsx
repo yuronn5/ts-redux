440086f09083dad8fe13d2462a4710e618809d1b
yurii bilous <yurii.b@elogic.com.ua>
2024-09-15 11:30:56 +0200

    example of reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { userSlice } from "./store/reducers/UserSlice";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
 
 function App() {
-  return <></>;
+  const { count } = useAppSelector((state) => state.userReducer);
+  const { increment } = userSlice.actions;
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment(5))}>Increment</button>
+    </div>
+  );
+  
 }
 
 export default App;
