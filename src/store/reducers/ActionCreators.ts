1ebfd8ac58bc0c2e543c457463e19c0823dbddf9
yurii bilous <yurii.b@elogic.com.ua>
2024-09-16 19:20:12 +0200

    default scenario with dispatches

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { AppDispatch } from "../store";
 import { IUser } from "../../models/IUser";
+import { userSlice } from "../reducers/UserSlice";
 
 
 export const fetchUsers = () => async (dispatch: AppDispatch)  => {
     try {
+        dispatch(userSlice.actions.usersFetching())
         const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+        dispatch(userSlice.actions.usersFetchingSuccess(response.data))
     } catch (error) {
-        console.log(error)
+        dispatch(userSlice.actions.usersFetchingError(error.message))
     }
 }
