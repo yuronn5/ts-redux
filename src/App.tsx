348faf9eff2a0bfaafc96822633908b041e59c6b
yurii bilous <yurii.b@elogic.com.ua>
2024-09-16 18:28:42 +0200

    removed example

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import { userSlice } from "./store/reducers/UserSlice";
 import { useAppDispatch, useAppSelector } from "./hooks/redux";
 
 function App() {
-  const { count } = useAppSelector((state) => state.userReducer);
-  const { increment } = userSlice.actions;
   const dispatch = useAppDispatch();
 
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment(5))}>Increment</button>
+
     </div>
   );
   
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -5,23 +5,19 @@ interface UserState {
     users: IUser[];
     isLoading: boolean;
     error: string;
-    count: number;
 }
 
 const initialState: UserState = {
     users: [],
     isLoading: false,
     error: '',
-    count: 0
 }
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        increment(state, action: PayloadAction<number>) {
-            state.count += action.payload
-        }
+        
     }
 })
 
