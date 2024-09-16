f276d4946f665d12e84b92f8416da8b7032555dd
yurii bilous <yurii.b@elogic.com.ua>
2024-09-16 21:47:00 +0200

    added normal data load, load, loading with error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import { userSlice } from "./store/reducers/UserSlice";
 import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import { useEffect } from "react";
+import { fetchUsers } from "./store/reducers/ActionCreators";
 
 function App() {
   const dispatch = useAppDispatch();
+  const { users, isLoading, error } = useAppSelector(state => state.userReducer);
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [dispatch])
 
   return (
     <div className="App">
-
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {JSON.stringify(users, null, 2)}
     </div>
   );
   
