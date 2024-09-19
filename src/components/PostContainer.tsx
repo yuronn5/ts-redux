bb1ed604a24d0dc87bbb9802b702394ad2c17da3
yurii bilous <yurii.b@elogic.com.ua>
2024-09-19 17:50:06 +0200

    loading and error

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -3,10 +3,12 @@ import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
 
 const PostContainer = () => {
-    const {data: posts} = postAPI.useFetchAllPostsQuery(5)
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(5)
   return (
     <div>
         <div className="posts_list">
+            {isLoading && <h1>Loading...</h1>}
+            {error && <h1>Error...</h1>}
             {posts && posts.map(post => <PostItem post={post} key={post.id} />)}
         </div>
     </div>
